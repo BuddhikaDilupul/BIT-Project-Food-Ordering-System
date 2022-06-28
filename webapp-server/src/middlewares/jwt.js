a5d3ad095d254a77a3814249673520c522a9033f
Buddhika D Ranaweera <106642348+BuddhikaDilupul@users.noreply.github.com>
2022-06-28 11:41:09 +0530

    Update jwt.js

--- webapp-server/src/middlewares/jwt.js
+++ webapp-server/src/middlewares/jwt.js
@@ -11,6 +11,7 @@ function authjwt() {
       { url: /\/api\/product(.*)/, methods: ['GET', 'OPTIONS'] }, //regx url for multiurl
       { url: /\/api\/category(.*)/, methods: ['GET', 'OPTIONS'] },
       { url: /\/api\/reset/, methods: ['POST', 'OPTIONS'] },
+      { url: /\/api\/customer/, methods: ['POST', 'OPTIONS'] },
       '/api/staff/login',
       '/api/staff/resetPassword',
       '/api/customer/login',
